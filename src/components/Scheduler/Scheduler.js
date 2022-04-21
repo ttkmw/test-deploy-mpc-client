5428e7e5c1432d538390af36d0dbad8f8003724b
Bell <gunn.kim@plabfootball.com>
2022-04-21 16:10:50 +0900

    Update Scheduler component to include multiple child components

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -3,9 +3,25 @@ import React from 'react';
 import classes from './Scheduler.module.css';
 
 const Scheduler = (props) => {
+  const addPropsToReactElement = (element, props) => {
+    if (React.isValidElement(element)) {
+      return React.cloneElement(element, props);
+    }
+    return element;
+  };
+
+  const addPropsToChildren = (children, props) => {
+    if (!Array.isArray(children)) {
+      return addPropsToReactElement(children, props);
+    }
+    return children.map((childElement, idx) =>
+      addPropsToReactElement(childElement, { key: idx, ...props })
+    );
+  };
+
   return (
     <div className={classes.scheduler}>
-      {React.cloneElement(props.children, { ...props })}
+      {addPropsToChildren(props.children, props)}
     </div>
   );
 };
